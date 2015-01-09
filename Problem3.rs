967e8e2c6a4ecaf037d25f80a4ba7a7f93e4de9d
~ <karimellaisy@hotmail.com>
2015-01-09 13:04:26 -0500

    Create Problem3.rs

---
+++ Problem3.rs
@@ -0,0 +1,21 @@
+fn main(){
+  let (mut _i,mut _j,mut _k) = (0i, 0i, 0i);
+  let num = 600851475143f64;
+  while _i<=num as isize{
+      _k=0;
+      if num%_i as f64==0f64{
+         _j=1;
+          while _j<=_i{
+            if _i%_j==0 as isize{
+                 _k += 1;
+                 }
+             _j+=1;
+          }
+          if _k==2 {
+             println!("{} is a prime factor",_i);
+            }
+      }
+      _i+=1;
+   }
+   return;
+}
