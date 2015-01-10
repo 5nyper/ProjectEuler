9a4c5c005c79808d9fd026607696932dc69acbac
Tupshin Harper <tupshin@tupshin.com>
2015-01-10 16:33:00 -0500

    a bit more rustic

--- Problem2.rs
+++ Problem2.rs
@@ -3,10 +3,10 @@ fn main() {
     let mut a = 0us;
     let mut v = vec!(1is , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55);
     for _i in [0is..23is].iter() {
-        let x = (v[v.len() - 2] + v[v.len() - 1]) as isize;
+        let x = v[v.len() - 2] + v[v.len() - 1];
         v.push(x);
     }
-    for _i in [0is..v.len() as isize].iter() {
+    for _i in [0us..v.len()].iter() {
         if v[a] % 2 == 0 { x += v[a]; }
         a += 1;
     } 
--- Problem6.rs
+++ Problem6.rs
@@ -5,13 +5,13 @@ fn main() {
     let mut sqa = 0is;
     let mut nat = 0.0;
     for _ in [0is..101is].iter() {
-    sqa += a.powi(2) as isize;
-    a += 1.0;
-}
-a = 0.0;
+        sqa += a.powi(2) as isize;
+        a += 1.0;
+    }
+    a = 0.0;
     for _ in [0is..100is].iter() {
-    a += 1.0;
-    nat += a;
+        a += 1.0;
+        nat += a;
     }
     println!("{}", nat.powi(2) as isize - sqa);
 }
