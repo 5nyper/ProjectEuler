0015bfab0eca053ae617bfaf57915f3e741a8db2
~ <karimellaisy@hotmail.com>
2015-01-10 20:56:33 -0500

    Added Florob's code

--- Problem2.rs
+++ Problem2.rs
@@ -1,3 +1,20 @@
+//Idiomatic Code
+ use std::iter::AdditiveIterator;
+-use std::num::Float;
+ 
+-fn is_prime(x: u64) -> bool {
+-    if x < 2 { return false; }
+-    if x == 2 { return true; }
+-    for i in 2..((x as f64).sqrt().ceil() as u64 + 1) {
+-        if x % i == 0 { return false; }
+-    }
+-    true
+-}
+-
+-fn main() {
+-    println!("{}", (2u64..2_000_000).filter(|&x| is_prime(x)).sum());
+}
+//Original Code
 fn main() {
     let mut x = 0is;
     let mut a = 0us;
