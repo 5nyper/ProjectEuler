4db504becf12b0470f26407aefe65fa5250536b4
~ <karimellaisy@hotmail.com>
2015-01-10 21:06:58 -0500

    Update Problem6.rs

--- Problem6.rs
+++ Problem6.rs
@@ -1,4 +1,14 @@
-use std::num::Float;
+///idiomatic code
+-use std::iter::AdditiveIterator;
+fn main() {
+-    let squares_sum = (1u32..101).map(|x| x * x).sum();
+-    let sum = (1u32..101).sum();
+-    let sum_squared = sum * sum;
+-
+-    println!("{}", sum_squared - squares_sum);
+}
+//Original Code
+se std::num::Float;
 
 fn main() {
     let mut a = 0.0;
