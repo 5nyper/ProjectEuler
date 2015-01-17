b6a84cb4b9267b4a304120da2d0f3ca3d7266a7c
~ <karimellaisy@hotmail.com>
2015-01-16 21:14:22 -0500

    Update Problem29.rs

--- src/Problem29.rs
+++ src/Problem29.rs
@@ -3,7 +3,6 @@ use num::pow;
 use num::bigint::ToBigInt;
 fn main() {
    let (mut num, mut pow) = (2i64, 2us);
-   let (mut a, mut b) = (0us, 1us);
    let mut li = vec![];
    loop {
       if pow == 101 && num == 100 {
@@ -18,13 +17,6 @@ fn main() {
       pow+=1;
    }
   li.sort();
-  for _ in (0..li.len() - 1) {
-        if li[a] == li[b] {
-            li.remove(b);
-        } else {
-            a+=1;
-            b+=1;
-        }
-    }
-  println!("{:?}", li.len()); 
+  li.dedup();
+  println!("{}", li.len()); 
 }
