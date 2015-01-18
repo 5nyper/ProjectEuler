e48b3da16124b41a581053938cdb701857d8c32f
~ <karimellaisy@hotmail.com>
2015-01-17 19:27:28 -0500

    Update Problem24.rs

--- src/Problem24.rs
+++ src/Problem24.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut v = vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut v = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for _ in (0us..999999us) { 
         v.next_permutation(); 
     }
