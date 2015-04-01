17428a9e5c1ee0add2f28b3a3a93ca8f5dc6cb02
a5i <Ap0ph1s@users.noreply.github.com>
2015-04-01 08:08:36 -0400

    Update Problem25.rs

--- src/Problem25.rs
+++ src/Problem25.rs
@@ -1,19 +1,3 @@
-//Original Code
-extern crate num;
-use num::{BigUint, Zero, One};
-use std::mem::replace;
- 
-fn fib(n: usize) -> usize {
-    let mut f0: BigUint = Zero::zero();
-    let mut f1: BigUint = One::one();
-    for _ in (0..n) {
-        let f2 = f0 + &f1;
-        // This is a low cost way of swapping f0 with f1 and f1 with f2.
-        f0 = replace(&mut f1, f2);
-    }
-    f0.to_string().len()
-}
- 
 fn main() {
 println!("fib(10000) = {}", fib(4782));
 }
